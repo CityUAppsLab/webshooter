83ffcfb17d25642afe6060b6aecd8150c3fd3967
kinni mew <kinni.mew@gmail.com>
2015-08-10 17:40:56 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+/**
+ * Created by kinnimew on 10/8/15.
+ */
+var config = require('./config');
+
+var webshooter = require('./webshooter');
+
+var dateFormat = require('dateFormat');
+var schedule = require('node-schedule');
+
+
+schedule.scheduleJob('*/' + config.interval + ' * * * *', function () {
+
+    var startTime = new Date();
+
+    console.log("job started at " + startTime);
+
+    var now = dateFormat(startTime, 'yyyymmddHHMM');
+    var outputFilePath = 'screenshots/' + now + '.png';
+
+    webshooter.takeScreenshot(config.url, outputFilePath, function (err) {
+
+        var endTime = new Date();
+
+        console.log("Elapsed Time = " + (endTime - startTime) + "ms");
+
+        var email = {
+            to: config.recipients
+        };
+
+        if (err) {
+
+            // Connection error
+            email.subject = 'Webshooter - ' + config.url;
+            email.text = "Connection Alert";
+
+        } else {
+
+            // Everything works fine
+            email.subject = 'Webshooter - ' + config.url;
+            email.text = "Elapsed Time = " + (endTime - startTime) + "ms";
+            email.attachment = [
+                {path: outputFilePath, name: now + ".png"}
+            ]
+        }
+
+        webshooter.send(email, function (err, result) {
+        });
+
+    })
+});
+
+
+console.log("Let's go! " + new Date());
---
+++ webshooter.js
@@ -0,0 +1,48 @@
+/**
+ * Created by kinnimew on 6/8/15.
+ */
+var config = require("./config");
+
+var webshot = require('webshot');
+var email = require('emailjs');
+
+// create eamil service
+var emailServer = email.server.connect({
+    user: config.smtp.username,
+    password: config.smtp.password,
+    host: config.smtp.host,
+    ssl: true
+});
+
+
+module.exports = {
+
+    /**
+     * This method is used to simulate a browser using WebKit from the Webshot module and save the screenshot.
+     *
+     * @param url The monitoring url.
+     * @param outputPath The path to store the screenshot.
+     * @param callback The callback when the process is done.
+     */
+    takeScreenshot: function (url, outputPath, callback) {
+        webshot(url, outputPath, function (err) {
+            callback(err);
+        });
+    },
+
+    /**
+     * This method is used to send email notification.
+     *
+     * @param email The email object containing the subject, message, sender, etc.
+     * @param callback The callback when the email is sent.
+     */
+    send: function (email, callback) {
+
+        // ensure sender address is set
+        email.from = config.senderAddress;
+
+        emailServer.send(email, function (err, message) {
+            callback(err, message);
+        });
+    }
+}
